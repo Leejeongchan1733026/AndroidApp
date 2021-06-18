98b17056e0872f38773708eabf010c102332459b
Leejeongchan1733026 <cksqls288@gmail.com>
2021-06-18 21:12:52 +0900

    add Buttom and Click Event

--- ButtomDemo/app/src/main/java/com/example/buttondemo/MainActivity.java
+++ ButtomDemo/app/src/main/java/com/example/buttondemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.buttondemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android
-
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick01 (View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void onClick02 (View v){
+        Toast.makeText(getApplicationContext(), "버튼 누름"), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClick03(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:00"));
+        startActivity(intent);
     }
 }
